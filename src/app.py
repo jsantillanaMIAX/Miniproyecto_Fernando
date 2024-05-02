fa7ec1e9ee086dc499d67d625a4b7f4b844af85b
jsantillanaMIAX <javier.santillana05e@gmail.com>
2024-05-02 19:02:22 +0200

    terminado

--- src/app.py
+++ src/app.py
@@ -1,13 +1,22 @@
 
 from fastapi import FastAPI
 import typing
+from pymongo import MongoClient
+import os
 
 app = FastAPI()
 
 
 @app.post("/insert_name")
 def insert_name(name: str):
+    #conectar al Mongo=mongodb://root:example@mongo:27017
+    mongo_dir = 'mongodb://root:example@mongo:27017'
+    client = MongoClient(mongo_dir)
+    db = client['DB_API']
+    col = db ['nombres']
+    col.insert_one({'name': name})
     pass
+
 
 @app.get("/names")
 def names() -> typing.List[str]:
